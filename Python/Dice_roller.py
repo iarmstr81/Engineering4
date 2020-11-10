bc713c8e0d4f9846d560cbd9a2667730ea322814
Imogen <iarmstr81@charlottesvilleschools.orggit config --global user.email iarmstr81@charlottesvilleschools.org>
2020-11-10 04:29:20 +0000

    Updates

--- Python/Dice_roller.py
+++ Python/Dice_roller.py
@@ -1,18 +1,21 @@
-# Automatic Dice Roller
-# Written by Imogen A
+#Automatic Dice Roller
+#Written by Imogen A
 
-import random
-import keyboard
+from random import randint
 import time
+import sys
 
 min = 1
 max = 6
-active= True
-print("Automatic Dice Roller")
-print("press ENTER to roll")
-print("press X to quit")
 
-while active == True:
-  if keyboard.is_pressed('enter'):
-    print(random.randint(min, max))
-    time.sleep(2)
+print ("Automatic Dice Roller")
+
+while True:
+	time.sleep(1)
+	RollPrompt = input("Press ENTER to Roll or X to quit: ")
+
+	if RollPrompt == (""):
+		print(randint(min,max))
+	if RollPrompt == ("x"):
+		sys.exit()
+
