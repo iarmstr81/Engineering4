afe943981092f5a300319f21b3200b453b8f9cf8
Imogen <iarmstr81@charlottesvilleschools.orggit config --global user.email iarmstr81@charlottesvilleschools.org>
2020-11-17 04:00:34 +0000

    Adding Calculator

---
+++ Calculator.py
@@ -0,0 +1,27 @@
+##Caluclator
+##Written by Imogen A
+
+input1 = input("Select First Number: ") 
+input2 = input("Select Second Number: ")
+
+def doMath(input1,input2,operator):
+	#Do Math Function, takes the 2 numbers, and decides which operator to use, and returns the value
+	if operator == 1: #sum
+		return int(input1) + int(input2)
+	if operator == 2: #difference
+		return int(input1) - int(input2)
+	if operator == 3: #product
+		return int(input1) * int(input2)
+	if operator == 4: #quotient
+		Q = int(input1) / int(input2) #finds quotient
+		RQ = round(Q,2) #rounds quotient to 2 places
+		return RQ
+	if operator == 5: #modulus
+		return int(input1) % int(input2)
+
+#prints the operator name, and the doMath result of that operator using the two user numbers
+print("Sum:\t\t" + str(doMath(input1,input2,1)))
+print("Difference:\t" + str(doMath(input1,input2,2)))
+print("Prodcut:\t" + str(doMath(input1,input2,3)))
+print("Quotient:\t" + str(doMath(input1,input2,4)))
+print("Modulo:\t\t" + str(doMath(input1,input2,5)))
--- Dice_roller.py
+++ Dice_roller.py
@@ -5,17 +5,17 @@ from random import randint
 import time
 import sys
 
-min = 1
-max = 6
+min = 1 #minimum number that you can get
+max = 6 #maximum number that you can get
 
-print ("Automatic Dice Roller")
+print ("Automatic Dice Roller") #Set up/title
 
 while True:
-	time.sleep(1)
-	RollPrompt = input("Press ENTER to Roll or X to quit: ")
+	time.sleep(1) #delay so it doesn't get overwhelmed
+	RollPrompt = input("Press ENTER to Roll or X to quit: ") #asking for input
 
-	if RollPrompt == (""):
-		print(randint(min,max))
-	if RollPrompt == ("x"):
-		sys.exit()
+	if RollPrompt == (""): #if enter is pressed
+		print(randint(min,max)) #print a number between 1 and 6
+	if RollPrompt == ("x"): #if x is pressed
+		sys.exit() #ends the program
 
