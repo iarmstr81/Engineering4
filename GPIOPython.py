71e3deeed026fe48c5e9d0af33e847832a76f6fc
Imogen <iarmstr81@charlottesvilleschools.orggit config --global user.email iarmstr81@charlottesvilleschools.org>
2021-01-29 02:10:07 +0000

    GPIO Python

---
+++ GPIOPython.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO 
+from time import sleep
+ 
+GPIO.setwarnings(False) # Everything breaks when this is removed
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(1, GPIO.OUT, initial=GPIO.LOW) # Pin 8 is an output
+
+for x in range(10):   #loop runs 10 times
+ GPIO.output(1, GPIO.HIGH) # Turn on
+ sleep(1) # Sleep for 1 second
+ GPIO.output(1, GPIO.LOW) # Turn off
+ sleep(1) # Sleep for 1 second
